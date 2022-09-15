f3f7fefa9a15ac8f24baabbbe8b0d30cf74f0fa8
Callistron <113582948+Callistron@users.noreply.github.com>
2022-09-15 09:11:41 +0200

    órai anyag 1
    objektum számláló

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace f2nafk12ora
+{
+    class szamol {
+        static int darab = 0;
+        public static int maxdarab = 0;
+        public szamol()
+        {
+            darab++;
+            Console.WriteLine(darab);
+            
+        }
+        ~szamol()
+        {
+            darab = darab - 1;
+        }
+    }
+
+    class Program
+
+    {
+            static void Main(string[] args)
+        {
+            for(int i=0;i<550000;i++)
+            {
+                new szamol();
+            }
+        }
+    }
+}
